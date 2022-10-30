5e2a9d12f485206aea9f1566592cd8172b56be36
Kajathees Premendran <60744394+kajan03@users.noreply.github.com>
2022-10-30 14:46:53 +0530

    jwt auth changes

---
+++ ssd_backend/server.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const jwt = require('jsonwebtoken');
+
+app.use(express.json());
+
+const posts = [
+    {
+        username:"Kajan",
+        title:"post 1"
+    },
+    {
+        username:"parathan",
+        title:"post 2"
+    }
+]
+
+app.get("/posts",authenticateToken,(req,res) => {
+    res.json(posts.filter(posts => posts.username ===req.user.name));
+})
+
+app.post('/login', (req,res) => {
+    //Authenticate user
+
+    const username = req.body.username;
+    const user = {name:username}
+
+    const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET )
+    res.json({accessToken:accessToken})
+})
+
+function authenticateToken(req,res,next){
+const authHeader = req.headers['authorization'];
+const token = authHeader && authHeader.split(' ')[1];
+if(token == null) return res.sendStatus(401)
+
+jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user) => {
+    if(err) return res.sendStatus(403)
+    req.user = user
+    next()
+})
+}
+
+app.listen(5000);
